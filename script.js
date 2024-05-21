f9693f2a7d11a45efb10cf46ee13125d7a735fbe
Subin Dev <nguyenhuutri.me+1@gmail.com>
2024-05-21 19:09:54 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-function getParameterByName(name, url = window.location.href) {
-    name = name.replace(/[\[\]]/g, '\\$&');
-    let regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
-        results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, ' '));
-}
 
-const message = getParameterByName('msg');
-
-if (message) {
-    document.getElementById('message').innerText = `Message received: ${message}`;
-} else {
-    document.getElementById('message').innerText = 'No message provided';
-}
