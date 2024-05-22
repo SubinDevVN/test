81c88dc3c42d3f31b5b29fd87e26f34220a48bb3
Subin Dev <nguyenhuutri.me+1@gmail.com>
2024-05-22 14:09:10 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // public/script.js
 document.getElementById('generateKey').addEventListener('click', async () => {
-    const response = await fetch('https://subindevvn.github.io/test/random'); // Thay 'your-server-url' bằng URL của máy chủ
+    const response = await fetch('https://subindevvn.github.io/test/'); // Thay 'your-server-url' bằng URL của máy chủ
     const data = await response.json();
     document.getElementById('keyContainer').innerText = JSON.stringify(data, null, 2);
 });
